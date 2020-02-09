25fbf02271300d7bfb50477f2896aa219b8d07a9
NikunjPatel31 <59450797+NikunjPatel31@users.noreply.github.com>
2020-02-09 21:27:52 +0530

    14/01/2020                                                                                                                                    Notification activity UI is ready.                                                                                                                                     navigation button listener is implemented.                                                                                                                                     creating profile fragment.                                                                                                                                   Profile fragment created successfully. creating account fragment.                                                                                                                                     account fragment created successfully  Logout function is implemented successfully.  Profile image is successfully uploaded into the firebase storage.  account screen is completely ready.  edit profile is completely ready.  add car is completely ready.  View_My_Cars_Activity added.  car_details_row is ready.  view_my_cars_activity is read.  edit_car_details is ready completely.
                                                                                                                                            delete car is implemented successfully.
    
                                                                                                                                            source_location_activity is in progress.
    
                                                                                                                                            icon at the center of the screen is added and app is getting the coordinates at that point.
    
    destination location activity is implemented.
    
    offer ride business logic is ready. database logic is remaining to implement.
    style for edit text is made.
    
    details of ride is uploaded successfully to the database.

--- ShareRide/app/src/main/java/com/example/shareride/Offer_ride_second_Activity.java
+++ ShareRide/app/src/main/java/com/example/shareride/Offer_ride_second_Activity.java
@@ -3,12 +3,17 @@ package com.example.shareride;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Offer_ride_second_Activity extends AppCompatActivity {
 
@@ -20,18 +25,19 @@ public class Offer_ride_second_Activity extends AppCompatActivity {
     private int counter = 0;
     private LatLng sourceLocation, destinationLocation;
     private String car_id, time, date;
+    private String costPerSeat, totalSeats;
+
+    private FirebaseAuth mAtuh;
+    private DatabaseReference mdatabaseReference;
+    private String UID;
 
     public void offer(View view)
     {
-        String costPerSeat = costPerSeatET.getText().toString();
-        String totalSeats = numOfSeats.getText().toString();
-        Log.d(TAG, "offer: Source Lat: "+sourceLocation.latitude+" Long: "+sourceLocation.longitude);
-        Log.d(TAG, "offer: Destination Lat: "+destinationLocation.latitude+" Long: "+destinationLocation.longitude);
-        Log.d(TAG, "offer: time: "+time);
-        Log.d(TAG, "offer: date: "+date);
-        Log.d(TAG, "offer: car_id: "+car_id);
-        Log.d(TAG, "offer: seat: "+totalSeats);
-        Log.d(TAG, "offer: cost: "+costPerSeat);
+        if(validatingFields())
+        {
+            Log.d(TAG, "offer: fields are validated.");
+            sendingDataToDatabase();
+        }
     }
 
     public void increaseNumber(View view)
@@ -56,12 +62,19 @@ public class Offer_ride_second_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer_ride_second_);
 
+        gettingIntentData();
+        initializeWidgets();
+        intializeFirebaseInstances();
+    }
+
+    private void gettingIntentData()
+    {
+        Log.d(TAG, "gettingIntentData: getting intent data.");
         sourceLocation = getIntent().getExtras().getParcelable("Source Location");
         destinationLocation = getIntent().getExtras().getParcelable("Destination Location");
         car_id = getIntent().getStringExtra("Car_id");
         time = getIntent().getStringExtra("Time");
         date = getIntent().getStringExtra("Date");
-        initializeWidgets();
     }
 
     private void initializeWidgets()
@@ -69,5 +82,59 @@ public class Offer_ride_second_Activity extends AppCompatActivity {
         Log.d(TAG, "initializeWidgets: initializing widgets");
         numOfSeats = (TextView) findViewById(R.id.number_of_seats_textview);
         costPerSeatET = (EditText) findViewById(R.id.cost_per_seat_edittext);
+    }
+
+    private void intializeFirebaseInstances()
+    {
+        Log.d(TAG, "intializeFirebaseInstances: initializing firebase instances. ");
+        mAtuh = FirebaseAuth.getInstance();
+        UID = mAtuh.getUid();
+        mdatabaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    private boolean validatingFields()
+    {
+        costPerSeat = costPerSeatET.getText().toString();
+        totalSeats = numOfSeats.getText().toString();
+
+        if(TextUtils.isEmpty(costPerSeat))
+        {
+            Toast.makeText(this, "Field is empty.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else
+        {
+            if(Integer.parseInt(totalSeats) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                Toast.makeText(this, "Number of seats cannot be zero.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+    }
+
+    private void sendingDataToDatabase()
+    {
+        Log.d(TAG, "sendingDataToDatabase: sending data to database.");
+        Log.d(TAG, "sendingDataToDatabase: Source Lat: "+sourceLocation.latitude+" Long: "+sourceLocation.longitude);
+        Log.d(TAG, "sendingDataToDatabase: Destination Lat: "+destinationLocation.latitude+" Long: "+destinationLocation.longitude);
+        Log.d(TAG, "sendingDataToDatabase: time: "+time);
+        Log.d(TAG, "sendingDataToDatabase: date: "+date);
+        Log.d(TAG, "sendingDataToDatabase: car_id: "+car_id);
+        Log.d(TAG, "sendingDataToDatabase: seat: "+totalSeats);
+        Log.d(TAG, "sendingDataToDatabase: cost: "+costPerSeat);
+        DatabaseReference mChildDB = mdatabaseReference.child("Offer_Ride").child(UID).push();
+        String sourceLocationCord = sourceLocation.latitude + "," + sourceLocation.longitude;
+        String destinationLocationCord = destinationLocation.latitude + "," + destinationLocation.longitude;
+        mChildDB.child("Source_Location").setValue(sourceLocationCord);
+        mChildDB.child("Destination_Location").setValue(destinationLocationCord);
+        mChildDB.child("Car_id").setValue(car_id);
+        mChildDB.child("Time").setValue(time);
+        mChildDB.child("Date").setValue(date);
+        mChildDB.child("Num_Seats").setValue(totalSeats);
+        mChildDB.child("Cost_Per_Seat").setValue(costPerSeat);
     }
 }
---
+++ ShareRide/app/src/main/java/com/example/shareride/Offered_Ride_Activity.java
@@ -0,0 +1,14 @@
+package com.example.shareride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Offered_Ride_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offered__ride_);
+    }
+}
